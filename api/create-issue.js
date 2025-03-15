a0d6aca2d53b3e487d7f6d4e2a5c7727341039e7
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-03-15 19:57:38 +0100

    feat: added feedback button and update footer links

---
+++ api/create-issue.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const { title, body, labels } = req.body;
+
+  const response = await fetch(
+    "https://api.github.com/repos/BennoCrafter/Timetable/issues",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `token ${GITHUB_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, body, labels }),
+    },
+  );
+
+  const data = await response.json();
+  return res.status(response.status).json(data);
+}
